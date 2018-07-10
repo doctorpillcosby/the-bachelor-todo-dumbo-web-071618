bf8f112a213ecee5ef4c8bd7104c71481db9bf3a
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-10 18:19:37 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,82 +1,41 @@
-
-	 def get_first_name_of_season_winner(data, season)
-   # code here	   # code here
-
- 
-    data[season].each do |people|
-
-      people.each do |k,v|
-        if v == "Winner"
-            full_name = people["name"]
-            return full_name.split(' ').first
-        end 
-     end
- end	 
- 	 
-
-
-	 def get_contestant_name(data, occupation)
-
-  data.each do |season, array|
-    array.each do |people|
-      people.each do |k,v|
-        if v == occupation
-          return people["name"]
-        end
-      end
-    end
-  end
-   # code here	   # code here
- 
- 	 
-
-
-
- def count_contestants_by_hometown(data, hometown)
-   # code here	   # code here
-counter = 0
-  data.each do |season, array|
-   array.each do |hash|
-      hash.each do |k,v|
-       if v == hometown
-          counter += 1
-        end
-      end
-    end
-counter
-
- end	 
- 	 
-
-
-
-	 def get_occupation(data, hometown)
-   # code here	   # code here
-  data.each do |season, array|
-    array.each do |hash|
-      hash.each do |k,v|
-        if v == hometown
-          return hash["occupation"]
-        end
-      end
-    end
-
+def get_first_name_of_season_winner(data,season)
+ data[season.to_sym].each do |into_array|
+   if into_array[:status] =="Winner"
+     return into_array[:name].split(" ")[0]
+   end
  end
- 	 
-	 def get_average_age_for_season(data, season)
-   # code here	   # code here
-age = 0
-count = 0
-  data[season].each do |hash|
-    hash.each do |k,v|
-      if k == "age"
-        count += 1
-        age += v.to_f
-      end
-  end
+end
 
-  answer = (age/count).round
-return answer
- end	 
+def get_contestant_name(data, occupation)
+data.each do |season, season_array|
+  season_array.each do |into_array|
+    if into_array[:occupation] == occupation
+      return into_array[:name]
+      end 
+    end 
+  end 
+end
 
+def count_contestants_by_hometown(data, hometown)
+  counter = 0
+ data.each do |season, season_array|
+    season_array.each do |into_array|
+      if into_array[:hometown] == hometown
+        counter+=1
+      end 
+  end 
+ end 
+ return counter 
+end
+
+def get_occupation(data, hometown)
+  data.each do |season, season_array|
+    season_array.each do |into_array|
+      if into_array[:hometown] == hometown
+        return into_array[:name]
+      end 
+  end 
+end
+
+get_occupation(data,"New York, New York")
 
