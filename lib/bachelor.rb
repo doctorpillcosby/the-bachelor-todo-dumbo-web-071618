920ca20442123fef52a4f9ac033ccbbf83d21e57
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-10 18:16:15 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -18,53 +18,53 @@
 +
 +
  def get_contestant_name(data, occupation)	 def get_contestant_name(data, occupation)
-+
-+  data.each do |season, array|
-+    array.each do |people|
-+      people.each do |k,v|
-+        if v == occupation
-+          return people["name"]
-+        end
-+      end
-+    end
-+  end
+
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |k,v|
+        if v == occupation
+          return people["name"]
+        end
+      end
+    end
+  end
    # code here	   # code here
  end	 end
  	 
-+
-+
-+
- def count_contestants_by_hometown(data, hometown)	 def count_contestants_by_hometown(data, hometown)
+
+
+
+ def count_contestants_by_hometown(data, hometown)
    # code here	   # code here
-+counter = 0
-+  data.each do |season, array|
-+    array.each do |hash|
-+      hash.each do |k,v|
-+        if v == hometown
-+          counter += 1
-+        end
-+      end
-+    end
-+  end
-+counter
-+
+counter = 0
+  data.each do |season, array|
+   array.each do |hash|
+      hash.each do |k,v|
+       if v == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+counter
+
  end	 end
  	 
-+
-+
-+
- def get_occupation(data, hometown)	 def get_occupation(data, hometown)
+
+
+
+	 def get_occupation(data, hometown)
    # code here	   # code here
-+  data.each do |season, array|
-+    array.each do |hash|
-+      hash.each do |k,v|
-+        if v == hometown
-+          return hash["occupation"]
-+        end
-+      end
-+    end
-+  end
-+
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end
+
  end	 end
  	 
 	 def get_average_age_for_season(data, season)
