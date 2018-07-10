b9c1782935bfccce2b340e2dad57c8f96ad6b53f
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-10 18:33:08 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -66,19 +66,6 @@ def count_contestants_by_hometown(data, hometown)
 
 end
 
-def get_occupation(data, hometown)
-  # code here
-  person = nil
-  data.each do |hash_season, contestants_array|
-    contestants_array.each do |contestant|
-      if contestant["hometown"] == hometown
-        person = contestant["occupation"]
-        
-      end
-    end
-  end
-  person
-end
 
 
 
