55f53c5938c0d749bfca0ef025d89495720b9edb
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-10 18:17:02 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,9 @@
 
- def get_first_name_of_season_winner(data, season)	 def get_first_name_of_season_winner(data, season)
+	 def get_first_name_of_season_winner(data, season)
    # code here	   # code here
 
-+  
-+    data[season].each do |people|
+ 
+    data[season].each do |people|
 
       people.each do |k,v|
         if v == "Winner"
